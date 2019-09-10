807562bfdc15b54725a6c42f159b8e0ac905da87
dujianwu123 <dujianwu123@126.com>
2019-09-10 11:25:51 +0800

    理财页

---
+++ app/js/money/index.vue
@@ -0,0 +1,50 @@
+<template lang="html">
+    <div>
+      <Heador></Heador>
+      <Mslider></Mslider>
+      <Ada></Ada>
+      <Money></Money>
+      <Adb></Adb>
+      <Finance></Finance>
+      <Adc></Adc>
+      <Footor cname="mfooter"></Footor>
+      <Navbar></Navbar>
+    </div>
+</template>
+
+<script>
+import Heador from "../public/header";
+import Footor from "../public/footer.vue";
+import Navbar from "../public/navbar.vue";
+import Mslider from "./mslider";
+import Ada from "./ada";
+import Adb from "./adb";
+import Money from "./money";
+import Finance from "./finance";
+import Adc from "./adc";
+export default {
+  components: {
+    Heador,
+    Footor,
+    Navbar,
+    Mslider,
+    Ada,
+    Adb,
+    Money,
+    Finance,
+    Adc
+  }
+};
+</script>
+
+<style lang="scss">
+.mfooter {
+  margin-top: 0 !important;
+  ul {
+    li:first-child {
+      display: none;
+    }
+  }
+}
+</style>
+
---
+++ app/js/money/mslider.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <Slider :items="items" cname="slider"></Slider>
+  </div>
+</template>
+
+<script>
+import Slider from "../core/slider";
+export default {
+  data() {
+    return {
+      items: [
+        {
+          href: "home",
+          src:
+            "//img12.360buyimg.com/jrpmobile/jfs/t11149/181/856008463/68797/dc303e9/59f97baeN3cdae084.jpg?width=750&height=400"
+        },
+        {
+          href: "home",
+          src:
+            "//img12.360buyimg.com/jrpmobile/jfs/t9994/205/2575467053/189117/125e7b05/59f97c36N7599be57.png?width=750&height=400"
+        }
+      ]
+    };
+  },
+  components: {
+    Slider
+  }
+};
+</script>
+<style lang="scss">
+.slider {
+  margin-top: 120px;
+  img {
+    width: 100%;
+  }
+  .swiper-pagination-bullet {
+    box-sizing: border-box;
+    border-radius: 7px;
+    height: 3px;
+    width: 8px;
+    background: rgba(248, 241, 241, 0.4);
+    margin: 0 2px;
+    display: inline-block;
+    opacity: 1;
+  }
+  .swiper-pagination-bullet-active {
+    background: #c7c1c1;
+    width: 12px;
+  }
+}
+</style>
+<style lang="scss" module>
+@import "../../css/reset.scss";
+</style>
